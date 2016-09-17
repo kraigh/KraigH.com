e34aff7235e05ff35eebe97f833c6d8e83df9bde
Kraig Hufstedler <kraigory@gmail.com>
2016-09-17 16:06:02 -0500

    fixed array issue

--- cse104/reviews.php
+++ cse104/reviews.php
@@ -4,7 +4,11 @@ if (!empty($_POST)) {
     $data[] = $_POST;
     $allResults = file_get_contents('results.json');
     $tempArray = json_decode($allResults);
-    array_push($tempArray, $data);
+    if is_array($tempArray) {
+        array_push($tempArray, $data);
+    } else {
+        $tempArray = $data;
+    }
     $jsonData = json_encode($tempArray);
     file_put_contents('results.json', $jsonData);
     // header('Location: http://kraigh.com/cse104/index.html?success=1');
