ca5a1c1ccf4f3c82f1a5d9033587f29902e59606
Kraig Hufstedler <kraigory@gmail.com>
2016-09-18 18:18:27 -0500

    each

--- cse104/js/main.js
+++ cse104/js/main.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function ($) {
     $.getJSON("http://kraigh.com/cse104/reviews.php", function(data) {
-        data.each(function(index, el) {
-            $('#reviews-list').add('<li>'+el.name+'</li>');
+        $.$.each(data, function(index, val) {
+            $('#reviews-list').append('<li>'+val.name+'</li>');
         });
     });
 });
