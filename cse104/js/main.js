bcc234277b062ac7c81a600b08e1442fdc983b41
Kraig Hufstedler <kraigory@gmail.com>
2016-09-18 18:39:59 -0500

    debug

--- cse104/js/main.js
+++ cse104/js/main.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function ($) {
     $.getJSON("http://kraigh.com/cse104/reviews.php", function(data) {
         $.each(data, function(index, val) {
+            console.log(val);
             var starsHtml = '';
             for (var i = 0; i <= 5; i++) {
                 if (i > data.rating) {
