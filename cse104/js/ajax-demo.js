6c834203164cfa97dc4b9a65247a3a4a6ff6e178
Kraig Hufstedler <kraigory@gmail.com>
2016-12-01 01:17:51 -0600

    submission form

--- cse104/js/ajax-demo.js
+++ cse104/js/ajax-demo.js
@@ -23,21 +23,17 @@ xhttp.send();
 // Add an event listener for submitting the form
 document.getElementById("review-form").addEventListener("submit", submitForm);
 
-function submitForm(event) {
-    event.preventDefault();
+function submitForm() {
+    // Prevent the default form submission
+    this.preventDefault();
 
+    // Get form values using "this" as a reference to the form
+    var name = this.elements.namedItem("name").value;
+    var menu = this.elements.namedItem("menu").value;
+    var greeted = this.elements.namedItem("greeted[]").value;
+    var rating = this.elements.namedItem("rating").value;
+    var comments = this.elements.namedItem("comments").value;
 
-    var name = document.getElementById("review-form").elements.namedItem("name").value;
-    var menu = document.getElementById("review-form").elements.namedItem("menu").value;
-    var greeted = document.getElementById("review-form").elements.namedItem("greeted[]").value;
-    var rating = document.getElementById("review-form").elements.namedItem("rating").value;
-    var comments = document.getElementById("review-form").elements.namedItem("comments").value;
-
-    // console.log(name);
-    // console.log(menu);
-    // console.log(greeted);
-    // console.log(rating);
-    // console.log(comments);
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         console.log(this.readyState);
@@ -45,12 +41,9 @@ function submitForm(event) {
            console.log(this.responseText);
         }
     };
-
     xhttp.open("POST", "reviews.php", true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send("?name=" + name + "&menu=" + menu + "&greeted=" + greeted + "&rating=" + rating + "&comments=" + comments);
-
-
+    xhttp.send("name=" + name + "&menu=" + menu + "&greeted=" + greeted + "&rating=" + rating + "&comments=" + comments);
 }
 
 
