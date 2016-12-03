4a4d8027950dab03adc9279f993923b897771c15
Kraig Hufstedler <kraigory@gmail.com>
2016-12-02 20:10:35 -0600

    better handling of no api key

--- cse104/recommendations-app/routes/recommendations.js
+++ cse104/recommendations-app/routes/recommendations.js
@@ -8,27 +8,28 @@ router.get('/', function(req, res, next) {
         console.log('No api key passed');
         res.status(406);
         res.send("Error: No API key passed.");
-    }
-    var MongoClient = require('mongodb').MongoClient
-    MongoClient.connect('mongodb://localhost:27017', function (err, db) {
-        if (err) throw err
-
-
-        db.collection('users').findOne({'apikey': req.query.api_key}, function (err, user) {
+    } else {
+        var MongoClient = require('mongodb').MongoClient
+        MongoClient.connect('mongodb://localhost:27017', function (err, db) {
             if (err) throw err
-            if (!user) {
-                console.log('No user found');
-                res.status(406);
-                res.send("Error: Invalid API key (no user found)");
-            } else {
-                db.collection('recommendations').find({'users_id': user._id}).toArray(function (err, recs) {
-                    if (err) throw err
-                    console.log(recs);
-                    res.send(recs);
-                });
-            }
+
+
+            db.collection('users').findOne({'apikey': req.query.api_key}, function (err, user) {
+                if (err) throw err
+                if (!user) {
+                    console.log('No user found');
+                    res.status(406);
+                    res.send("Error: Invalid API key (no user found)");
+                } else {
+                    db.collection('recommendations').find({'users_id': user._id}).toArray(function (err, recs) {
+                        if (err) throw err
+                        console.log(recs);
+                        res.send(recs);
+                    });
+                }
+            });
         });
-    });
+    }
 
 });
 
