53f3416de117c47f4a2a38116669d143ec013223
Kraig Hufstedler <kraigory@gmail.com>
2017-08-30 09:49:51 -0500

    added some comments

--- cse104/recommendations-app/routes/recommendations.js
+++ cse104/recommendations-app/routes/recommendations.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-
+// Get request
 router.get('/', function(req, res, next) {
+    // API key is required to determine the user who is requesting the data
     if (typeof(req.query.api_key) == 'undefined') {
         console.log('No api key passed');
         res.status(406);
@@ -12,7 +13,7 @@ router.get('/', function(req, res, next) {
         MongoClient.connect('mongodb://localhost:27017', function (err, db) {
             if (err) throw err
 
-
+            // Find user who the api key belongs to
             db.collection('users').findOne({'apikey': req.query.api_key}, function (err, user) {
                 if (err) throw err
                 if (!user) {
@@ -20,6 +21,7 @@ router.get('/', function(req, res, next) {
                     res.status(406);
                     res.send("Error: Invalid API key (no user found)");
                 } else {
+                    // Find recommendations belonging to the user
                     db.collection('recommendations').find({'users_id': user._id}).toArray(function (err, recs) {
                         if (err) throw err
                         console.log(recs);
@@ -32,8 +34,9 @@ router.get('/', function(req, res, next) {
 
 });
 
-
+// POST request
 router.post('/', function(req, res, next) {
+    // API key required to determine the user to save the recommendations for
     if (typeof(req.body.api_key) == 'undefined') {
         console.log('No api key passed');
         console.log(req.body);
@@ -51,7 +54,7 @@ router.post('/', function(req, res, next) {
         var MongoClient = require('mongodb').MongoClient
         MongoClient.connect('mongodb://localhost:27017', function (err, db) {
             if (err) throw err
-
+            // Find user for the API key
             db.collection('users').findOne({'apikey': req.body.api_key}, function (err, user) {
                 if (err) throw err
                 console.log(user);
@@ -60,9 +63,11 @@ router.post('/', function(req, res, next) {
                     res.status(404);
                     res.send("Error: incorrect API key, no user found");
                 } else {
+                    // Add submitted recommendation with the correct user id
                     db.collection('recommendations').insertOne({'users_id': user._id, 'name': req.body.name, 'text': req.body.text}, function (err, result) {
                         if (err) throw err
                         console.log(result.ops[0]);
+                        // Return the submitted data that has been successfully stored (this is so the new review can be rendered)
                         res.send(result.ops[0]);
                     });
                 }
